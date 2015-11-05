38e852a58c1e6c8de3259453652a27e64a505315
Pete Otaqui <pete@otaqui.com>
2015-11-05 07:19:46 +0000

    renamed index.js to rear-window.js

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'index.js',
+      'rear-window.js',
       'test/**/*-spec.js',
       {pattern: 'test/style-*.css', included: false}
     ],
